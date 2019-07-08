dd839d3acd12b2d3a930def2a7fc8c5c244000fd
Emily Pancake <epancake24@gmail.com>
2019-07-07 21:41:16 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fetch = require("node-fetch");
+var cache = require("express-redis-cache")();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+apiUrl = 'https://api.github.com/search/repositories'
+
+
+app.get("/:sortMethod/:searchterm", cache.route(), (request, response) => {  
+  fetch(apiUrl + `?q=${request.params.searchterm}&per_page=50&sort=${request.params.sortMethod}`)
+    .then(response => response.json())
+    .then(data => response.send({ data }))
+    .catch(err => console.log("error"));
+});
+
+app.listen((process.env.PORT || 4000), () => {
+  console.log("Repo Depot listening on port 4000!");
+});
